b8f6aa7b8e503d2c07d01874f6660d7a6d4ac0b5
Laura Garcia <Laura.Garcia@endava.com>
2022-08-10 17:33:51 -0500

    Change button

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'New Item'}
+                        {submitting ? 'Adding...' : 'My new Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
